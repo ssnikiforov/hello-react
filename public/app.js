0c68eafb52b5997a48c434c7e10a5fdc3e3b8755
ssnikiforov <stepan001@yandex.ru>
2017-04-29 12:03:30 +0300

    04. Add webpack and some dependencies, without config file

---
+++ public/app.js
@@ -0,0 +1,6 @@
+/**
+ * Created by snikiforov on 28/04/2017.
+ */
+var greeter = require('./components/Greeter');
+
+greeter();
---
+++ public/components/Greeter.js
@@ -0,0 +1,8 @@
+/**
+ * Created by snikiforov on 28/04/2017.
+ */
+function greeter () {
+    document.write('From greeter function');
+}
+
+module.exports = greeter;
