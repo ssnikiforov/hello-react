5050383c35561a758ae2252927da3bb0679e36d8
ssnikiforov <stepan001@yandex.ru>
2017-04-29 16:06:50 +0300

    Migrate to latest versions of dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -12,13 +12,16 @@ module.exports = {
         filename: './public/bundle.js'
     },
     resolve: {
-        root: __dirname,
+        modules: [
+            __dirname,
+            "node_modules"
+        ],
         alias: {
             Greeter: 'public/components/Greeter.jsx',
             GreeterMessage: 'public/components/GreeterMessage.jsx',
             GreeterForm: 'public/components/GreeterForm.jsx',
         },
-        extensions: ['', '.js', '.jsx']
+        extensions: ['.js', '.jsx']
     },
     module: {
         loaders: [
